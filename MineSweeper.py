1a406ffeca88268ab140db6c1ac3505ce8233330
Simon Lay <lay00214@outlook.com>
2018-07-01 21:44:41 +1000

    added some comments and deleted a few variables

--- MineSweeper.py
+++ MineSweeper.py
@@ -8,20 +8,19 @@ class MineSweeper:
 	coordsX=[];
 	coordsY=[];
 	point=[0,0];
-	pointX=-1;
-	pointY=-1;
 	discovered =[];
-	bomb = np.zeros((width,height));
+	graph = np.zeros((width,height));
+	
+	#clears the screen
 	def cls(self):
 		os.system('cls')
-		
+	
+	#initializes the bombs position and amount of bombs for the game
 	def bombs(self,coordsX,coordsY,height,width):
 		count =0;
-		
 		while count < height and count < width:
 			count = (random.randint(1,width*height));
 		#print(count);
-		
 		for num in range(count):
 			positionX = random.randint(1,width-1);
 			positionY = random.randint(1,height-1);
@@ -31,45 +30,45 @@ class MineSweeper:
 					positionY = random.randint(1,height-1);
 			coordsX.insert(len(coordsX),positionX);
 			coordsY.insert(len(coordsY),positionY);
-		#print(str(coordsX),"",str(coordsY));
-		self.bomb[coordsX,coordsY] = 1; 
-		#print(self.bomb);
-	
+		self.graph[coordsX,coordsY] = 1; 
+
+	#represents the entirety of the game to the player
 	def gameboard(self,coordsX,coordsY,height,width):
 		x = coordsX;
 		y = coordsY;
-		if self.pointX >=0 and self.pointY >=0:
-			self.bomb[self.pointX,self.pointY] = 2;
+		if int(self.point[0]) >=0 and int(self.point[1]) >=0:
+			self.graph[int(self.point[0]),int(self.point[1])] = 2;
 			for row in range(width):
 				for column in range(height):
-					if self.pointX == row and self.pointY == column:
+					if int(self.point[0]) == row and int(self.point[1]) == column:
 					#right
 						for i in range(height-column):
-							if self.bomb[row,column+i] == 1 and self.bomb[row,column+i-1] == 0:
-								self.bomb[row,column+i-1] = 3;
-							if self.bomb[row,column+i] == 1:
+							if self.graph[row,column+i] == 1 and self.graph[row,column+i-1] == 0:
+								self.graph[row,column+i-1] = 3;
+							if self.graph[row,column+i] == 1:
 								break;
 					#left	
 						for i in range(column):
-							if self.bomb[row,column-i] == 1 and self.bomb[row,column-i+1] == 0:
-								self.bomb[row,column-i+1] = 3;
-							if self.bomb[row,column-i] == 1:
+							if self.graph[row,column-i] == 1 and self.graph[row,column-i+1] == 0:
+								self.graph[row,column-i+1] = 3;
+							if self.graph[row,column-i] == 1:
 								break;
 					#up
 						for i in range(width-row):
-							if self.bomb[row+i,column] == 1 and self.bomb[row+i-1,column] == 0:
-								self.bomb[row+i-1,column] = 3;
-							if self.bomb[row+i,column] == 1:
+							if self.graph[row+i,column] == 1 and self.graph[row+i-1,column] == 0:
+								self.graph[row+i-1,column] = 3;
+							if self.graph[row+i,column] == 1:
 								break;
 					#down
 						for i in range(row):
-							if self.bomb[row-i,column] == 1 and self.bomb[row-i+1,column] == 0:
-								self.bomb[row-i+1,column] = 3;
-							if self.bomb[row-i,column] == 1:
+							if self.graph[row-i,column] == 1 and self.graph[row-i+1,column] == 0:
+								self.graph[row-i+1,column] = 3;
+							if self.graph[row-i,column] == 1:
 								break;
 						break;		
-		print(self.bomb);
+		print(self.graph);
 		
+		#draws the board from the graph
 		print(" "*4,end='');
 		for i in range(width):
 			print("|",i,"",end='');
@@ -82,28 +81,26 @@ class MineSweeper:
 				print(row,end='   ');
 			for column in range(height):
 				print('|',end='');
-				if self.bomb[row,column] == 0:
+				if self.graph[row,column] == 0:
 					print('   ',end='');
-				elif self.bomb[row,column] == 1:
+				elif self.graph[row,column] == 1:
 					print(' B ',end='');
-				elif self.bomb[row,column] == 2:
+				elif self.graph[row,column] == 2:
 					print(' V ',end='');
-				elif self.bomb[row,column] == 3:
+				elif self.graph[row,column] == 3:
 					print(' N ',end='');
 				#replace this with the number of bombs surrounding it
 			print('|');
 		
-
+	#gets player's input
 	def turn(self):
 		print('insert coordinates. (IE. row,column)');
 		user = input('>');
-		##self.cls();
+		self.cls();
 		
 		#get the coords then plot the section
 		self.point = user.split(',');
-		self.pointX = int(self.point[0]);
-		self.pointY = int(self.point[1]);
-		if self.pointX>=0 and self.pointY <self.width+1:
+		if int(self.point[0])>=0 and int(self.point[1])<self.width+1:
 			self.gameboard(self.coordsX,self.coordsY,self.height,self.width);
 				
 m = MineSweeper();
